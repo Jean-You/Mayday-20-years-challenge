bfe00491e060b433d24e83f4685e1591f96e2b4e
Jean You <jeanyou1012@gmail.com>
2021-02-06 22:14:11 +0800

    Initial Commit

---
+++ Mayday 20 years challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mayday 20 years challenge
+//
+//  Created by Jeanyou on 2021/2/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
